60273ee71ec28f10ed5ce5bba19edf06118a7eab
Nabir14 <mushfiquefarhannabir@gmail.com>
2024-08-04 09:22:06 +0000

    fix

--- scripts/cross-compiler-script/cc.sh
+++ scripts/cross-compiler-script/cc.sh
@@ -9,25 +9,25 @@ sudo apt install libisl-dev
 
 export CC=/usr/bin/gcc 
 export LD=/usr/bin/gcc 
-export PREFIX="$Home/cc-bin" 
+export PREFIX="/usr/local/i386elfgcc"
 export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
 
 mkdir /tmp/src 
 cd /tmp/src 
-curl -O https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz 
-tar xf binutils-2.38.tar.gz
+curl -O https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.gz 
+tar xf binutils-2.40.tar.gz
 mkdir binutils-build
 cd binutils-build
-../binutils-2.38/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+../binutils-2.40/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
 make all install 2>&1 | tee make.log
 
 cd /tmp/src 
-curl -O https://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz 
-tar xf gcc-11.4.0.tar.gz 
+curl -O https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz 
+tar xf gcc-12.2.0.tar.gz 
 mkdir gcc-build
 cd gcc-build
-../gcc-11.4.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-languages=c --without-headers
+../gcc-12.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-languages=c --without-headers
 make all-gcc 
 make all-target-libgcc 
 make install-gcc
