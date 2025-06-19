d8fd0c3edfb508acf78d83d0c9b8908322dedb86
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-06-19 15:35:47 +0600

    191

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,7 +1,17 @@
-void test_ep(){
-}
+#define VMEM_ADDR 0xb8000
 
 void main(){
-	char* vmem = (char*) 0xb8000;
-	*vmem = 'X';
+	char* vmem = (char*) VMEM_ADDR;
+	*vmem[0x0] = 'AmpereOS v0.2';
+	*vmem[0x2] = 0x0f;
+	clear_screen();
 }
+
+void clea_screen(){
+	for(int i = 0; i < 80 * 25; i++){
+		char* vmem = (char*) VMEM_ADDR;
+		*vmem[i * 2] = ' ';
+		*vmem[(i * 2) + 1] = 0x0f;
+	}
+}
+
