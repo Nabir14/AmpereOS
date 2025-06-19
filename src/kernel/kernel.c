e200093e854e87d7d7f010eaec203d464cee8a27
MD Mushfique Farhan Nabir <82253045+Nabir14@users.noreply.github.com>
2025-06-19 10:42:24 +0000

    shit

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,8 +2,7 @@
 
 void main(){
 	char* vmem = (char*) VMEM_ADDR;
-	*vmem[0x0] = 'AmpereOS v0.2';
-	*vmem[0x2] = 0x0f;
+	*vmem = 'AmpereOS v0.2';
 	clear_screen();
 }
 
@@ -11,7 +10,6 @@ void clea_screen(){
 	for(int i = 0; i < 80 * 25; i++){
 		char* vmem = (char*) VMEM_ADDR;
 		*vmem[i * 2] = ' ';
-		*vmem[(i * 2) + 1] = 0x0f;
 	}
 }
 
