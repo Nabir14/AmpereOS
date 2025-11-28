31b8a1d401dfb627d5eff1e202108b72a08a0dd6
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-11-28 21:20:07 +0600

    clean

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
-all: build-image run
+all: cc-compile-bootloader cc-compile-kernel build-image run
 cc-compile-bootloader:
-	nasm -f bin /workspaces/AmpereOS/src/bootloader/bootloader.asm -o /workspaces/AmpereOS/bootloader.bin
+	nasm -fbin src/bootloader/bootloader.asm -o bootloader.bin -I src/bootloader/
 cc-compile-kernel:
-	./scripts/cross-compiler/bin/i686-elf-gcc -ffreestanding -c ./src/kernel/kernel.c -o ./src/kernel.o
-	./scripts/cross-compiler/bin/i686-elf-gcc -ffreestanding -c ./src/kernel/drivers/io-ports.c -o ./src/io-ports.o
-	./scripts/cross-compiler/bin/i686-elf-gcc -ffreestanding -c ./src/kernel/drivers/io-text.c -o ./src/io-text.o
-	./scripts/cross-compiler/bin/i686-elf-gcc -ffreestanding -c ./src/kernel/libc/mem.c -o ./src/mem.o
+	./scripts/cross-compiler/bin/i386-elf-gcc -ffreestanding -c ./src/kernel/kernel.c -o ./src/kernel.o
+	./scripts/cross-compiler/bin/i386-elf-gcc -ffreestanding -c ./src/kernel/drivers/io-ports.c -o ./src/io-ports.o
+	./scripts/cross-compiler/bin/i386-elf-gcc -ffreestanding -c ./src/kernel/drivers/io-text.c -o ./src/io-text.o
+	./scripts/cross-compiler/bin/i386-elf-gcc -ffreestanding -c ./src/kernel/libc/mem.c -o ./src/mem.o
 	nasm ./src/kernel/kernel_ep.asm -f elf -o ./src/kernel_ep.o
-	./scripts/cross-compiler/bin/i686-elf-ld -o kernel.bin -Ttext 0x1000 ./src/kernel_ep.o ./src/kernel.o ./src/io-ports.o ./src/io-text.o ./src/mem.o --oformat binary
+	./scripts/cross-compiler/bin/i386-elf-ld -o kernel.bin -Ttext 0x1000 ./src/kernel_ep.o ./src/kernel.o ./src/io-ports.o ./src/io-text.o ./src/mem.o --oformat binary
 build-image:
 	cat bootloader.bin kernel.bin > ./os-image/ampereos-dev.bin
 run:
 	qemu-system-i386 -hda os-image/ampereos-dev.bin
 clean:
-	rm -rf ./src/kernel.o ./src/kernel_ep.o ./src/io-ports.o ./src/io-text.o ./src/mem.o
+	rm -rf ./src/kernel.o ./src/kernel_ep.o ./src/io-ports.o ./src/io-text.o ./src/mem.o kernel.bin bootloader.bin os-image/ampereos-dev.bin
--- scripts/cross-compiler.sh
+++ scripts/cross-compiler.sh
@@ -1,34 +1,36 @@
-sudo apt install build-essential
-sudo apt install bison
-sudo apt install flex
-sudo apt install libgmp3-dev
-sudo apt install libmpc-dev
-sudo apt install libmpfr-dev
-sudo apt install texinfo
-sudo apt install libisl-dev
+sudo dnf install gcc gcc-c++ make
+sudo dnf install bison
+sudo dnf install flex
+sudo dnf install gmp-devel
+sudo dnf install libmpc-devel
+sudo dnf install mpfr-devel
+sudo dnf install texinfo
+sudo dnf install isl-devel
+sudo dnf install gawk
 
-export PREFIX="/workspaces/AmpereOS/scripts/cross-compiler"
-export TARGET=i686-elf
+export PREFIX=$HOME/code/AmpereOS/scripts/cross-compiler
+export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
 
 mkdir cross-compiler
 cd cross-compiler
-curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.gz
-tar xf binutils-2.42.tar.gz
-rm -rf binutils-2.42.tar.gz
+curl -O https://ftp.gnu.org/gnu/binutils/binutils-2.45.tar.gz
+tar xf binutils-2.45.tar.gz
+rm -rf binutils-2.45.tar.gz
 mkdir build-binutils
 cd build-binutils
-../binutils-2.42/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+../binutils-2.45/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
 make all install 2>&1 | tee make.log
 
 cd ..
-curl -O https://ftp.gnu.org/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.gz
-tar xf gcc-13.3.0.tar.gz
-rm -rf gcc-13.3.0.tar.gz
+curl -O https://ftp.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.gz
+tar xf gcc-15.2.0.tar.gz
+rm -rf gcc-15.2.0.tar.gz
 mkdir build-gcc
 cd build-gcc
-../gcc-13.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-languages=c --without-headers
-make all-gcc 
+../gcc-15.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-languages=c --without-headers
+
+imake all-gcc 
 make all-target-libgcc 
 make install-gcc 
 make install-target-libgcc
